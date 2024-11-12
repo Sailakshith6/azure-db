389bd4e55f7c4028731743821bf55032cbf3d482
Sailakshith6 <117815467+Sailakshith6@users.noreply.github.com>
2024-11-12 16:28:57 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "mysql_server_name" {
+  description = "The name of the MySQL server"
+  value       = azurerm_mysql_server.example.name
+}
+
+output "mysql_database_name" {
+  description = "The name of the MySQL database"
+  value       = azurerm_mysql_database.example.name
+}
+
+output "mysql_server_fqdn" {
+  description = "The FQDN of the MySQL server"
+  value       = azurerm_mysql_server.example.fqdn
+}
